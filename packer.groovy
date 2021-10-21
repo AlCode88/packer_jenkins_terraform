2de5fbe2141ff77f0a95625abd5292a153822921
AlCode88 <alanburkanov@gmail.com>
2021-10-20 19:46:03 -0500

    added stages

--- packer.groovy
+++ packer.groovy
@@ -1,6 +1,8 @@
 node('worker1'){
     stage('Install Apache'){
-          git 'https://github.com/AlCode88/packer_jenkins_terraform.git'
-        sh 'ls'
+        sh '''
+            git pull
+            sh 'ls'
+        '''
     }
 }
