e3cf4f418e97d75f50ddfb2053996ae79ee2b7af
AlCode88 <alanburkanov@gmail.com>
2021-10-20 21:07:57 -0500

    added file

--- packer.groovy
+++ packer.groovy
@@ -5,11 +5,16 @@ node('worker1'){
             git pull -u origin main
         '''
     }
-    withEnv(['REGION=us-east-1', 'PACKER_AMI_NAME=packerimage1']) {
+    withEnv(['REGION=us-east-1', 'PACKER_AMI_NAME=packerimage-{{timestamp}}']) {
         withCredentials([usernamePassword(credentialsId: 'aws-jenkins-user', passwordVariable: 'AWS_SECRET_ACCESS_KEY', usernameVariable: 'AWS_ACCESS_KEY_ID')]) {
-            stage('List all the files'){
+            stage('Validate Packer'){
                 sh '''
                     packer validate packer.json
+                '''
+            }
+            stage('Validate built'){
+                sh '''
+                    packer build packer.json
                 '''
             }
         }
