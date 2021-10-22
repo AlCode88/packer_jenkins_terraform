564010e965433b4b4c9e0c7a52fb9a488b1fedef
AlCode88 <alanburkanov@gmail.com>
2021-10-22 20:51:42 +0000

    added file

--- packer.groovy
+++ packer.groovy
@@ -1,6 +1,7 @@
 properties([
     parameters([
-        choice(choices: ['dev', 'qa', 'prod'], description: 'Select environment to apply', name: 'env'), 
+        choice(choices: ['dev', 'qa', 'prod'], description: 'Select environment to apply', name: 'env'),
+        choice(choices: ['us-east-1', 'us-east-2'], description: 'Select region', name: 'region') 
     ])
 ])
 
@@ -15,7 +16,14 @@ else{
     environment="prod"
 }
 
-#image_name=$params.env-packer-{timestamp}
+if(params.region == 'us-east-1'){
+    region="us-east-1"
+}
+else {
+   region="us-east-2"
+}
+
+//image_name=$params.env-packer-{timestamp}
 
 node('worker1'){
     stage('Install Apache'){
@@ -24,7 +32,7 @@ node('worker1'){
             ls
         '''
     }
-    withEnv(["REGION=us-east-1", "PACKER_AMI_NAME=$environment"]) {
+    withEnv(["REGION=$region", "PACKER_AMI_NAME=$environment"]) {
         withCredentials([usernamePassword(credentialsId: 'aws-jenkins-user', passwordVariable: 'AWS_SECRET_ACCESS_KEY', usernameVariable: 'AWS_ACCESS_KEY_ID')]) {
             stage('Validate Packer'){
                 sh '''
